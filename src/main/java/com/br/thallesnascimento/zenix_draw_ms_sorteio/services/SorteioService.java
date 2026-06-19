d31b77fc0e26aecad0a93b13fee6914ab0ba378c
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-06-19 16:24:22 -0300

    refactor: ajustei a forma de separar os times e ajustei para quantos times o cliente vai querer

--- src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/services/SorteioService.java
+++ src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/services/SorteioService.java
@@ -5,40 +5,30 @@ import com.br.thallesnascimento.zenix_draw_ms_sorteio.dtos.ListaDTOResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SorteioService {
 
     public ListaDTOResponse sortearTimes(ListaDTO listaDTO){
-        List<String> lista = listaDTO.nomes();
+        int numeroTimes = listaDTO.numeroTimes();
+        Map<String, List<String>> times = new HashMap<>();
 
-        if (lista.size() % 2f == 0){
-            List<String> time1 = new ArrayList<>();
-            List<String> time2 = new ArrayList<>();
+        int qtdJogadoresPorTime = listaDTO.jogadores().size() / numeroTimes;
+        for (int i = 0; i < numeroTimes; i++) {
+            List<String> jogadoresPorTime = new ArrayList<>(qtdJogadoresPorTime);
 
-            for (int i = 0; i <= lista.size() - 1 ; i+=2) {
-                time1.add(lista.get(i));
-                time2.add(lista.get(i + 1));
+            for (int j = 0; j < qtdJogadoresPorTime; j++) {
+                jogadoresPorTime.add(listaDTO.jogadores().getFirst());
+                listaDTO.jogadores().removeFirst();
             }
 
-            return new ListaDTOResponse(time1, time2, null);
+            times.put("time" + (i+1), jogadoresPorTime);
 
         }
-        else if (lista.size() % 3f == 0){
-            List<String> time1 = new ArrayList<>();
-            List<String> time2 = new ArrayList<>();
-            List<String> time3 = new ArrayList<>();
-
-            for (int i = 0; i < lista.size() - 1 ; i+=3) {
-                time1.add(lista.get(i));
-                time2.add(lista.get(i + 1));
-                time3.add(lista.get(i + 2));
-            }
-
-            return new ListaDTOResponse(time1, time2, time3);
-        }
-        return null;
+        return new ListaDTOResponse(times);
     }
 
 }
