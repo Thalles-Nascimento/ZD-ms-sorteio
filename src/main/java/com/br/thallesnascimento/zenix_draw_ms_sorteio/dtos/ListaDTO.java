4342db2afee1fb9393455db00f8a9cfbfabadd1d
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-06-19 16:22:28 -0300

    refactor: ajustando o nome da lista de jogadores e adicionando o numero de times no envio da requisicao

--- src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/dtos/ListaDTO.java
+++ src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/dtos/ListaDTO.java
@@ -2,5 +2,5 @@ package com.br.thallesnascimento.zenix_draw_ms_sorteio.dtos;
 
 import java.util.List;
 
-public record ListaDTO(List<String> nomes) {
+public record ListaDTO(List<String> jogadores, int numeroTimes) {
 }
