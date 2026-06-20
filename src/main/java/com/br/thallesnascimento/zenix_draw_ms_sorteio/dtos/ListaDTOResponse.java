152f38effd4b476884ad84f4d1883ddb82c57290
Thalles Nascimento <thalles.nascimentorj@gmail.com>
2026-06-20 14:29:36 -0300

    feat: adicionado na resposta da requisicao o numero de jogadores por time e se ha reservas

--- src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/dtos/ListaDTOResponse.java
+++ src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/dtos/ListaDTOResponse.java
@@ -3,5 +3,5 @@ package com.br.thallesnascimento.zenix_draw_ms_sorteio.dtos;
 import java.util.List;
 import java.util.Map;
 
-public record ListaDTOResponse(Map<String, List<String>> times) {
+public record ListaDTOResponse(Map<String, List<String>> times, List<String> reservas, int jogadoresPorTime) {
 }
