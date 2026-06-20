00538a4a4cf5657f040f775673da2b04d51d790b
Thalles Nascimento <thalles.nascimentorj@gmail.com>
2026-06-20 16:17:03 -0300

    refactor: ajustando os dtos para a nova classe jogadores

--- src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/dtos/ListaDTO.java
+++ src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/dtos/ListaDTO.java
@@ -1,6 +1,8 @@
 package com.br.thallesnascimento.zenix_draw_ms_sorteio.dtos;
 
+import com.br.thallesnascimento.zenix_draw_ms_sorteio.Jogadores;
+
 import java.util.List;
 
-public record ListaDTO(List<String> jogadores, int numeroTimes) {
+public record ListaDTO(List<Jogadores> jogadores, int numeroTimes) {
 }
--- src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/dtos/ListaDTOResponse.java
+++ src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/dtos/ListaDTOResponse.java
@@ -1,7 +1,10 @@
 package com.br.thallesnascimento.zenix_draw_ms_sorteio.dtos;
 
+import com.br.thallesnascimento.zenix_draw_ms_sorteio.Jogadores;
+import com.br.thallesnascimento.zenix_draw_ms_sorteio.enums.Posicao;
+
 import java.util.List;
 import java.util.Map;
 
-public record ListaDTOResponse(Map<String, List<String>> times, List<String> reservas, int jogadoresPorTime) {
+public record ListaDTOResponse(Map<String, List<Jogadores>> times, List<Jogadores> reservas, int jogadoresPorTime) {
 }
