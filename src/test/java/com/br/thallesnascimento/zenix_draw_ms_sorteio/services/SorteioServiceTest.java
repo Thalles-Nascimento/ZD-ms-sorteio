e523840dcbaca4ea87056868a3821cba0d6acc28
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-09-17 10:34:45 -0300

    refactor: Ajustando o nome do arquivo .jar criado no build

---
+++ src/test/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/services/SorteioServiceTest.java
@@ -0,0 +1,17 @@
+package com.br.thallesnascimento.zenix_draw_ms_sorteio.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SorteioServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void sortearTimes() {
+    }
+}
