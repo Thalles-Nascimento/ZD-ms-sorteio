e0e11acaa413a38d93a8975702cdfc064cc9ec41
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2026-09-17 11:56:41 -0300

    test: Refatorando os testes -> Implementado o padrão builder para o objeto Jogador e refazendo a lógica de verificação da lista ordenada

--- src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/models/entities/Jogador.java
+++ src/main/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/models/entities/Jogador.java
@@ -2,12 +2,14 @@ package com.br.thallesnascimento.zenix_draw_ms_sorteio.models.entities;
 
 import com.br.thallesnascimento.zenix_draw_ms_sorteio.models.enums.Posicao;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Jogadores {
     private String nome;
     private Posicao posicao;
--- src/test/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/services/SorteioServiceTest.java
+++ src/test/java/com/br/thallesnascimento/zenix_draw_ms_sorteio/services/SorteioServiceTest.java
@@ -2,17 +2,17 @@ package com.br.thallesnascimento.zenix_draw_ms_sorteio.services;
 
 import com.br.thallesnascimento.zenix_draw_ms_sorteio.models.dtos.ListaDTO;
 import com.br.thallesnascimento.zenix_draw_ms_sorteio.models.dtos.ListaDTOResponse;
-import com.br.thallesnascimento.zenix_draw_ms_sorteio.models.entities.Jogadores;
+import com.br.thallesnascimento.zenix_draw_ms_sorteio.models.entities.Jogador;
 import com.br.thallesnascimento.zenix_draw_ms_sorteio.models.enums.Posicao;
-import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class SorteioServiceTest {
@@ -22,23 +22,32 @@ class SorteioServiceTest {
     @BeforeEach
     void setUp() {
         sorteioService = new SorteioService();
-        Jogadores jogador1 = new Jogadores();
-        Jogadores jogador2 = new Jogadores();
-        Jogadores jogador3 = new Jogadores();
-        Jogadores jogador4 = new Jogadores();
-        jogador1.setNome("Thalles");
-        jogador1.setForca(5.2f);
-        jogador1.setPosicao(Posicao.ATACANTE);
-        jogador2.setNome("Gustavo");
-        jogador2.setForca(5.0f);
-        jogador2.setPosicao(Posicao.ZAGUEIRO);
-        jogador3.setNome("Jorlan");
-        jogador3.setForca(4.8f);
-        jogador3.setPosicao(Posicao.GOLEIRO);
-        jogador4.setNome("Mattheus");
-        jogador4.setForca(5.3f);
-        jogador4.setPosicao(Posicao.LATERAL);
-        List<Jogadores> listJogadores = new ArrayList<>();
+
+        // 1. Jogador
+        Jogador jogador1 = Jogador.builder()
+                .nome("Thalles")
+                .posicao(Posicao.ATACANTE)
+                .build();
+
+        // 2. Jogador
+        Jogador jogador2 = Jogador.builder()
+                .nome("Gustavo")
+                .posicao(Posicao.ZAGUEIRO)
+                .build();
+
+        // 3. Jogador
+        Jogador jogador3 = Jogador.builder()
+                .nome("Jorlan")
+                .posicao(Posicao.GOLEIRO)
+                .build();
+
+        // 4. Jogador
+        Jogador jogador4 = Jogador.builder()
+                .nome("Mattheus")
+                .posicao(Posicao.LATERAL)
+                .build();
+
+        List<Jogador> listJogadores = new ArrayList<>();
         listJogadores.add(jogador1);
         listJogadores.add(jogador2);
         listJogadores.add(jogador3);
@@ -57,17 +66,8 @@ class SorteioServiceTest {
     @Test
     void listaDeJogadoresDeveEstarOrdenadaPorPosicao() {
         ListaDTOResponse result = sorteioService.sortearTimes(jogadores);
-        assertTrue(ordenada(result.times().get("time1")), "Lista deve estar ordenada");
-        assertTrue(ordenada(result.times().get("time2")), "Lista deve estar ordenada");
+        assertThat(result.times().get("time1")).isSortedAccordingTo(Comparator.comparing(Jogador::getPosicao));
+        assertThat(result.times().get("time2")).isSortedAccordingTo(Comparator.comparing(Jogador::getPosicao));
 
-    }
-
-    private boolean ordenada(@NonNull List<Jogadores> list){
-        Jogadores jogador1 = list.getFirst();
-        Jogadores jogador2 = list.getLast();
-        System.out.println("Jogador 1 = " + jogador1);
-        System.out.println("Jogador 2 = " + jogador2);
-
-        return jogador2.getPosicao().ordinal() > jogador1.getPosicao().ordinal();
     }
 }
